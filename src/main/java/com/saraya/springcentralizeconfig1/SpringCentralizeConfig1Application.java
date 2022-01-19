1ae7d6c6ba85d0630f2122f68de5b9236834bf7c
ousmaneG <gassamaousmane@gmail.com>
2022-01-19 19:45:42 +0000

    first commit

---
+++ src/main/java/com/saraya/springcentralizeconfig1/SpringCentralizeConfig1Application.java
@@ -0,0 +1,13 @@
+package com.saraya.springcentralizeconfig1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCentralizeConfig1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCentralizeConfig1Application.class, args);
+	}
+
+}
